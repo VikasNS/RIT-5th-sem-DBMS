ec82c00f2b9a3f478389acae84c02d2f7e9fbc79
Vikas NS <nsvikas1998@gmail.com>
2018-12-28 21:39:23 +0530

    Update A52.js

--- A52.js
+++ A52.js
@@ -3,7 +3,7 @@ db.createCollection('BOOK')
 
 db.Book.insertMany(
 [
-  {'ISBN':123,'TITLE':'Oracle PL/SQL','AUTHOR':'Benjamin Rosenzweig','PUBLISHER':'Example','USN':'1MS16IS114','DATE':new Date("2015-01-25")},
+  {'ISBN':123,'TITLE':'Database','AUTHOR':'Benjamin Rosenzweig','PUBLISHER':'Example','USN':'1MS16IS114','DATE':new Date("2015-01-25")},
   {'ISBN':321,'TITLE':'Digital Image Processing','AUTHOR':'Pearson','PUBLISHER':'Rafael C.Gonzalez','USN':'1MS16IS114','DATE':new Date("2010-02-25")},
   {'ISBN':231,'TITLE':'Fundamentals of Digital Image Processing','AUTHOR':'Anil K. Jain','PUBLISHER':'Pearson Education','USN':'1MS16IS114','DATE':new Date("2014-02-05")},
   {'ISBN':132,'TITLE':'Natural Language Processing','AUTHOR':'Tanveer Siddiqui','PUBLISHER':'Oxford University','USN':'1MS17EE114','DATE':new Date("2013-05-25")},
@@ -13,6 +13,8 @@ db.Book.insertMany(
   {'ISBN':243,'TITLE':'Linear Algebra and its Applications','AUTHOR':'Gilbert Strang','PUBLISHER':'Thomson learning Asia','USN':'1MS18EC114','DATE':new Date("2016-03-15")}
 ]
 )
+db.Book.find({'ISBN':123})
+db.Book.find({'TITLE':'Database'})
 
 
 
